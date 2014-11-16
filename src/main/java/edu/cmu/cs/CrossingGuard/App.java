4e6083f48b5ffa8af3f4a6abeb352eba97712f20
TJGlazierCMU <tglazier@cs.cmu.edu>
2014-11-16 18:30:22 -0500

    Initial Commit

---
+++ src/main/java/edu/cmu/cs/CrossingGuard/App.java
@@ -0,0 +1,13 @@
+package edu.cmu.cs.CrossingGuard;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
